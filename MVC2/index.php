f63a43b1d8ad147c90f3bbde2094b6b8817e3e2b
CuriouSound <robin.nota68@gmail.com>
2018-12-03 13:55:31 +0100

    carousel non responsive

--- MVC2/index.php
+++ MVC2/index.php
@@ -13,16 +13,88 @@
             
     <div class="topSlider">
       
+        <!-- Slider 1 -->
         
-        <div class="slide"><p class="ecriture">hue</p></div>
+        <div class="slide">
+            <div class="carre1">
+                <img class="image" src="css/image/avenger.jpg"> 
+            </div>
 
-        <div class="slide"><p class="ecriture">bbbbb</p></div>
+            <div class="carre2">
+                <img class="image" src="css/image/batman.jpg"> 
+            </div>
 
-       
-        <div class="slide"><p class="ecriture">bbbbb</p></div>
+            <div class="carre3">
+                <img class="image" src="css/image/django.jpg"> 
+            </div>
 
-        <div class="slide"><p class="ecriture">bbbbb</p></div>
-    </div>
+            <div class="carre4">
+                <img class="image" src="css/image/equalizer.jpg"> 
+            </div>
+        </div>
+
+        <!-- Slider 2 -->
+
+        <div class="slide">
+            <div class="carre1">
+                <img class="image" src="css/image/harryPotter4.jpg"> 
+            </div>
+
+            <div class="carre2">
+                <img class="image" src="css/image/LaLigneVerte.jpg"> 
+            </div>
+
+            <div class="carre3">
+                <img class="image" src="css/image/LaReineDesNeiges.jpg"> 
+            </div>
+
+            <div class="carre4">
+                <img class="image" src="css/image/leNombre23.jpg"> 
+            </div>
+        </div>
+
+       <!-- Slider 3 -->
+
+       <div class="slide">
+            <div class="carre1">
+                <img class="image" src="css/image/lesBronzes.jpg"> 
+            </div>
+
+            <div class="carre2">
+                <img class="image" src="css/image/lordsOfWar.jpg"> 
+            </div>
+
+            <div class="carre3">
+                <img class="image" src="css/image/missionImpossible.jpg"> 
+            </div>
+
+            <div class="carre4">
+                <img class="image" src="css/image/pulpfiction.jpg"> 
+            </div>
+        </div>
+
+        <!-- Slider 4 -->
+
+        <div class="slide">
+           <h2 id="avenir">A VENIR PROCHAINEMENT :</h2>
+           <div class="carre1">
+                <img class="image" src="css/image/RobinDesBois.jpg"> 
+            </div>
+
+            <div class="carre2">
+                <img class="image" src="css/image/LeGrinch.jpg"> 
+            </div>
+
+            <div class="carre3">
+                <img class="image" src="css/image/BohemianRhapsody.jpg"> 
+            </div>
+
+            <div class="carre4">
+                <img class="image" src="css/image/LesAnimauxFantastiques2.jpg"> 
+            </div>
+        </div>
+
+
 </body>
 </html>
 <?php
