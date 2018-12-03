fd8ad6409aab5fd37b0d01d3e8f204e6275301d4
CuriouSound <robin.nota68@gmail.com>
2018-12-03 14:50:32 +0100

    ajout du dessus/dessous

--- MVC2/Views/HomeView.php
+++ MVC2/Views/HomeView.php
@@ -1,21 +1,107 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Annuaire de films - <?=$page?></title>
-    <link rel="stylesheet" href="Css/style.css">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Image Slider</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
 </head>
-
 <body>
-    <h1>
-        <?=$page?>
-    </h1>
-    <ul>
-        <?php foreach ($film as $film){ ?>                        
-           <?="<li><a href='film/" .$film['id']. "'>" .$film['titre']. "</a></li>" ?>
-        <?php } ?>
-    </ul>
+
+
+
+    
+<div class="imgdessus"></div>
+
+    <div class="allcontainer">
+    
+        <div class="topSlider">
+        
+            <!-- Slider 1 -->
+            
+            <div class="slide">
+                <div class="carre1">
+                    <img class="image" src="css/image/avenger.jpg"> 
+                </div>
+
+                <div class="carre2">
+                    <img class="image" src="css/image/batman.jpg"> 
+                </div>
+
+                <div class="carre3">
+                    <img class="image" src="css/image/django.jpg"> 
+                </div>
+
+                <div class="carre4">
+                    <img class="image" src="css/image/equalizer.jpg"> 
+                </div>
+            </div>
+
+            <!-- Slider 2 -->
+
+            <div class="slide">
+                <div class="carre1">
+                    <img class="image" src="css/image/harryPotter4.jpg"> 
+                </div>
+
+                <div class="carre2">
+                    <img class="image" src="css/image/LaLigneVerte.jpg"> 
+                </div>
+
+                <div class="carre3">
+                    <img class="image" src="css/image/LaReineDesNeiges.jpg"> 
+                </div>
+
+                <div class="carre4">
+                    <img class="image" src="css/image/leNombre23.jpg"> 
+                </div>
+            </div>
+
+        <!-- Slider 3 -->
+
+        <div class="slide">
+                <div class="carre1">
+                    <img class="image" src="css/image/lesBronzes.jpg"> 
+                </div>
+
+                <div class="carre2">
+                    <img class="image" src="css/image/lordsOfWar.jpg"> 
+                </div>
+
+                <div class="carre3">
+                    <img class="image" src="css/image/missionImpossible.jpg"> 
+                </div>
+
+                <div class="carre4">
+                    <img class="image" src="css/image/pulpfiction.jpg"> 
+                </div>
+            </div>
+
+            <!-- Slider 4 -->
+
+            <div class="slide">
+            <h2 id="avenir">A VENIR PROCHAINEMENT :</h2>
+            <div class="carre1">
+                    <img class="image" src="css/image/RobinDesBois.jpg"> 
+                </div>
+
+                <div class="carre2">
+                    <img class="image" src="css/image/LeGrinch.jpg"> 
+                </div>
+
+                <div class="carre3">
+                    <img class="image" src="css/image/BohemianRhapsody.jpg"> 
+                </div>
+
+                <div class="carre4">
+                    <img class="image" src="css/image/LesAnimauxFantastiques2.jpg"> 
+                </div>
+            </div>
+            
+
+
+</div>
+<div class="imgdessous"></div>
 </body>
 </html>
